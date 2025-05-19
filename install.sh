d576572bcd57fd9f1d98b084b6475ac9629342ce
AlcatrazTH <74221996+Alcatraz2033@users.noreply.github.com>
2025-05-19 15:40:35 -0500

    Actualización de las rutas de carpetas

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ sudo apt update -y && sudo apt upgrade -y
 echo -e "[${BLUE}+${END}] ${BLUE}Instalando Fuentes...${END}\n"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
 unzip Hack.zip
-mv *.ttf /usr/share/fonts
+mv *.ttf /usr/share/fonts/
 rm *.md
 
 echo -e "[${BLUE}+${END}] ${BLUE}Instalanto Kitty terminal...${END}\n"
@@ -54,7 +54,7 @@ sudo apt install kitty -y
 kitty &
 sleep 2
 pkill kitty
-mv kitty.conf ~/.config/kitty
+mv kitty.conf ~/.config/kitty/
 
 echo -e "[${BLUE}+${END}] ${BLUE}Instalando Power Level 10k...${END}\n"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -78,7 +78,7 @@ gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 
 echo -e "[${BLUE}+${END}] ${BLUE}Instalando barra de tareas...${END}\n"
 git clone https://github.com/Neeraj029/custom-xfce4-panel.git
-cd custom-xfce4-panel
+cd custom-xfce4-panel/
 cp everblush/gtk.css ~/.config/gtk-3.0/
 killall xfconfd
 cp xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
